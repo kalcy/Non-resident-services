ca5d3fa415a9b96f2540958fcb9b0dbbd001c723
Kalyan Kondi <kkondi@avery.com>
2015-06-12 13:18:44 -0700

    First Cut

---
+++ js/360-settings.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+$(document).ready(function(e) {
+	if($(window).width() < 360){
+    $('.container3').html('<center><img src="images/circule.png" alt="Print and Ship" class="circule"/></center>');
+	}
+});
